b4c3d55c3da5c53986518bbe1be77d30ea0600e3
Julian <julian.aschwanden@lernende.bbw.ch>
2025-01-22 18:21:55 +0100

    A bit of styling

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,33 @@ function App() {
 
     return (
         <>
-            <h1>Points Earned: {Math.floor(count)}</h1>
-            <h2>Box Clicker</h2>
-            <a className="button">
-                {Array.from( {length: amountOfButtons}).map((_, i) =>
+            <header>
+                <h1>Points Earned: {Math.floor(count)}</h1>
+                <h2>Box Clicker</h2>
+            </header>
+            <body>
+            <a>
+                {Array.from({length: amountOfButtons}).map((_, i) =>
                     <div>
                         <button key={i} className="button" onClick={() => setCount((count) => count + addedValue)}/>
-                        <button className={"buyButton"} onClick={() => setButtonLevel((buttonLevel) => buttonLevel + 1)}>
+                        <button className={"buyButton"}
+                                onClick={() => setButtonLevel((buttonLevel) => buttonLevel + 1)}>
                             Upgrade Button
                         </button>
                     </div>
                 )}
             </a>
-            <button className={"upgradeButton"} onClick={() => setAddedValue((addedValue) => buttonLevel/10 * addedValue + 1)}>
-                Upgrade
-            </button>
-            <button className={"buyButton"} onClick={() => setamountOfButtons((amountOfButtons) => amountOfButtons + 1)}>
-                Buy Box
-            </button>
+            </body>
+            <footer className={"menu"}>
+                <button className={"upgradeButton"}
+                        onClick={() => setAddedValue((addedValue) => buttonLevel / 2 * addedValue + 1)}>
+                    Upgrade
+                </button>
+                <button className={"buyButton"}
+                        onClick={() => setamountOfButtons((amountOfButtons) => amountOfButtons + 1)}>
+                    Buy Box
+                </button>
+            </footer>
         </>
     )
 }
